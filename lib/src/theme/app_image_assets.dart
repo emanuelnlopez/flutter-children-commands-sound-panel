ce05b333afaa1a415149c6065f1377dc88a807a5
Emanuel López <emanuel.n.lopez@gmail.com>
2023-04-24 18:48:20 -0300

    Adds images enum class

---
+++ lib/src/theme/app_image_assets.dart
@@ -0,0 +1,7 @@
+abstract class AppImageAssets {
+  static const manu1 = 'assets/images/manu1.png';
+  static const manu2 = 'assets/images/manu2.png';
+  static const manu3 = 'assets/images/manu3.png';
+
+  static const manuAnimationImages = [manu1, manu2, manu3, manu2];
+}
